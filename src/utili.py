f83b78e79bcb3e174f482085f4ef7452c179b08b
oleander_wsl <2024111155@stu.sufe.edu.cn>
2025-12-25 18:32:54 +0800

    refactor(src/utili.py): Update
    1. Rename Timers to MyTimer for better presentation.
    2. Remove useless helpers.

--- src/utili.py
+++ src/utili.py
@@ -1,16 +1,10 @@
-import datetime
-import logging
-import os
-import shutil
 from contextlib import contextmanager
-from pathlib import Path
 from typing import Any
 
 from ignite.handlers import Timer
-from torch.utils.tensorboard.writer import SummaryWriter
 
 
-class Timers:
+class MyTimer:
     def __init__(self):
         self._dict: dict[str, Timer] = {}
 
@@ -62,33 +56,6 @@ def make_conf(base: dict[str, Any], upd: str) -> dict[str, Any]:
         for k, v in base[upd].items():
             conf[k] = v
     return conf
-
-
-def make_logger(name: str, config_path: Path, logdir: Path) -> tuple[logging.Logger, SummaryWriter, Path]:
-    log_time = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
-    logdir = logdir / f"{log_time}_{name}"
-
-    os.makedirs(logdir)
-    os.makedirs(logdir / "checkpoints")
-
-    shutil.copy(config_path, logdir / config_path.name)
-
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-
-    handler = logging.FileHandler(logdir / "train.log")
-    handler.setLevel(logging.INFO)
-
-    formatter = logging.Formatter(
-        "[%(levelname)s %(asctime)s] %(message)s",
-        datefmt="%y%m%d %H:%M:%S",
-    )
-
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
-    writer = SummaryWriter(log_dir=logdir)
-    return logger, writer, logdir
 
 
 def make_table(columns: list[str], rows: list[list[Any]]) -> str:
